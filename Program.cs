4238f394e5faf630da43fc981a1e9278b4f48d26
Rafael Santos <rafa.05125@gmail.com>
2026-06-04 09:48:01 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+#pragma warning disable SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+var builder = Kernel.CreateBuilder().AddOllamaChatCompletion(
+    modelId: "llama3.1:latest",
+    endpoint: new Uri("http://localhost:11434")
+);
+
+builder.Services.AddLogging(x => x.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+var kernel = builder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
