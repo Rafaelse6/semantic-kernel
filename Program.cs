d9c82a5805418cd6c1ed45cbe61748521041b0f7
Rafael Santos <rafa.05125@gmail.com>
2026-06-04 10:01:34 -0300

    Ollama connection through semantic kernel

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.Ollama;
 
 #pragma warning disable SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 var builder = Kernel.CreateBuilder().AddOllamaChatCompletion(
@@ -9,7 +10,33 @@ var builder = Kernel.CreateBuilder().AddOllamaChatCompletion(
     endpoint: new Uri("http://localhost:11434")
 );
 
-builder.Services.AddLogging(x => x.AddConsole().SetMinimumLevel(LogLevel.Trace));
+builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
 
 var kernel = builder.Build();
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+OllamaPromptExecutionSettings settings = new()
+{
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
+
+var history = new ChatHistory();
+
+string? prompt;
+
+do
+{
+    Console.Write("User >");
+    prompt = Console.ReadLine();
+
+    history.AddUserMessage(prompt);
+
+    var result = await chatCompletionService.GetChatMessageContentAsync(
+         history,
+         executionSettings: settings,
+         kernel: kernel);
+
+    Console.WriteLine("Assistant > " + result);
+
+    history.AddMessage(result.Role, result.Content ?? string.Empty);
+} while (prompt is not null);
